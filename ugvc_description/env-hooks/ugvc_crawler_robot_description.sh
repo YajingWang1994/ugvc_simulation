2dcd7a65644b98e8f9d182c928faba92a55097a3
Yajing Wang <845217113@qq.com>
2016-05-24 21:35:05 +0800

    add ros_plugin to 4 main wheels

---
+++ ugvc_description/env-hooks/ugvc_crawler_robot_description.sh
@@ -0,0 +1,1 @@
+export UGVC_CRAWLER_ROBOT_DESCRIPTION=$(catkin_find --without-underlays --first-only ugvc_description urdf/ugvc_crawler_robot.xacro 2>/dev/null)
---
+++ ugvc_gazebo/env-hooks/kylin_gazebo.sh
@@ -0,0 +1,1 @@
+export KYLIN_GAZEBO_DESCRIPTION=$(catkin_find --without-underlays --first-only ugvc_gazebo urdf/description.gazebo.xacro 2>/dev/null)
